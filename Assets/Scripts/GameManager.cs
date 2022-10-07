4ede614245671ef60c2e2ae4ce09b33c8a9d54ed
Jamie J <111623045+JavisKitty@users.noreply.github.com>
2022-10-06 20:13:11 -0400

    Final Project

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region 
+    public static GameManager gm;
+    public Transform[] nodes;
+    void Awake()
+    {
+        if (!gm) gm = this;
+        else Destroy(this);
+    }
+    #endregion
+    public GameObject player;
+
+}
